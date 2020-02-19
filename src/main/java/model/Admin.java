e7920d65f6c4ff4d0616fd40fd720b62b1edcd50
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-02-19 19:42:56 +0000

    add Admin Class

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,41 @@
+package main.java.model;
+
+import java.util.List;
+
+public class Admin extends Employee {
+    private List<Teacher> teacherList;
+    private List<Course> classes;
+    private TeacherRequestMap teacherRequestMap;
+
+    public Admin(String name, List<Course> classes, TeachRequestMap teachRequestMap) {
+        super(name);
+        this.classes = classes;
+        this.teachRequestMap = teachRequestMap;
+    }
+
+    public List<Teacher> findSuitableStaff(Course course) {
+        List<Teacher> suitableTeacherList = null;
+        for (int i = 0; i < teacherList.size(); i++) {
+            if (teacherList.get(i).requirementsFulfilled(teacherList.get(i).getSkills())) {
+                suitableTeacherList.add(teacherList.get(i));
+            }
+        }
+        return suitableTeacherList;
+    }
+
+    public void proposeTeacher(Course course, Teacher teacher) {
+        teacherRequestMap.proposeTeacher(course, teacher);
+    }
+
+    public void addTraining(Teacher teacher, String training) {
+        teacher.addTraining(training);
+    }
+
+    public void addCourse(Course course) {
+        classes.add(course);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teacherList.add(teacher);
+    }
+}
