ca3bb4175fbe09a689e929e1e41e9e9497b1953f
Filip <2175499M@student.gla.ac.uk>
2020-02-22 15:43:30 +0000

    made the HashMap in TEachREquestMap private

--- src/main/java/model/TeachRequestMap.java
+++ src/main/java/model/TeachRequestMap.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
  */
 public class TeachRequestMap {
 
-    HashMap<String, TeachRequest> teachRequestMap;
+    private HashMap<String, TeachRequest> teachRequestMap;
 
     /**
      * Constructor.
